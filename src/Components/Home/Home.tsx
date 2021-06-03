fb30b4c6d89690fdce59fa0cbcc99e00d468e9ae
MSK0303 <yoshihara@ltas03.com>
2021-06-04 07:52:54 +0900

    #13 react-springのアニメーションでメッセージを変更する処理を実装

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,19 +2,48 @@ import React,{useState} from 'react'
 import Footer from '../Footer/Footer';
 import MyIcon from '../Parts/MyIcon';
 import "./Home.scss";
+//animation
+import {useTransition,animated,config} from 'react-spring';
 
 const Top:React.FC = () => {
     const message_table = ["Engineer","Blogger","Soccer Coach"];
     const [index, setIndex] = useState(0);
     const message = message_table[index];
 
+    // const message_conmps = [
+    //     ({style}) => (
+    //         <animated.div style={{color:"red",...style}}>
+    //             Engineer
+    //         </animated.div>
+    //     ),
+    //     ({style}) => (
+    //         <animated.div style={{color:"green",...style}}>
+    //             Blogger
+    //         </animated.div>
+    //     ),
+    //     ({style}) => (
+    //         <animated.div style={{color:"blue",...style}}>
+    //             Soccer Coach
+    //         </animated.div>
+    //     )
+    // ];
+
+    const transitions = useTransition(index,{
+        from: {opacity:0},
+        enter:{opacity:1},
+        leave:{opacity:0},
+        delay: 200,
+        config: config.molasses,
+        onRest: () => setIndex((index+1)%message_table.length),
+    });
+
     // //timer処理
-    setTimeout( () => {
-        let new_index = index+1;
-        if(new_index >= message_table.length) new_index = 0;
-        setIndex(new_index);
-        console.log("change");
-    },5000);
+    // setTimeout( () => {
+    //     let new_index = index+1;
+    //     if(new_index >= message_table.length) new_index = 0;
+    //     setIndex(new_index);
+    //     console.log("change");
+    // },5000);
 
     return (
         <div className="home-page">
@@ -22,7 +51,15 @@ const Top:React.FC = () => {
                 <p className="top-label">Welcome to MSK's Portfolio Site</p>
             </div>
             <MyIcon />
-            <p className="top-message">I'm <span className="role">{message}</span>.</p>
+            <p className="top-message">I'm <span className="role">{transitions(({opacity},item) => (
+                <animated.div style={{
+                    display: "inline-block",
+                    position: "absolute",
+                    opacity: opacity.to({range:[0.0,1.0],output:[0,1]}),
+                }}>
+                    {message_table[item]}
+                </animated.div>
+            ))}</span></p>
             <Footer />
         </div>
     )
