59906db80de5d16180fa44ef8d6677bc9f11518f
MSK0303 <yoshihara@ltas03.com>
2021-06-04 06:00:51 +0900

    #13 setTimeoutで表示内容の変更を実装

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Footer from '../Footer/Footer';
 import MyIcon from '../Parts/MyIcon';
 import "./Home.scss";
 
 const Top:React.FC = () => {
+    const message_table = ["Engineer","Blogger","Soccer Coach"];
+    const [index, setIndex] = useState(0);
+    const message = message_table[index];
+
+    // //timer処理
+    setTimeout( () => {
+        let new_index = index+1;
+        if(new_index >= message_table.length) new_index = 0;
+        setIndex(new_index);
+        console.log("change");
+    },5000);
+
     return (
         <div className="home-page">
             <div className="top" >
                 <p className="top-label">Welcome to MSK's Portfolio Site</p>
             </div>
             <MyIcon />
-            <p className="top-message">I'm <span className="role">Engineer</span>.</p>
+            <p className="top-message">I'm <span className="role">{message}</span>.</p>
             <Footer />
         </div>
     )
